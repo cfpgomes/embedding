069a9d269e532b9eec763de70afd66026872434a
Cláudio Gomes <cfpgcp3@gmail.com>
2021-03-20 00:24:52 +0000

    add histograms and scatters

--- src/compare_sets_of_samples/histograms_energy_vs_objective_value.py
+++ src/compare_sets_of_samples/histograms_energy_vs_objective_value.py
@@ -52,7 +52,7 @@ with open(set1_filename) as csvfile:
     for row in reader:
         # To append energy num_occur times.
         set1_samples_energy.extend([float(row[-2])
-                                   for i in range(int(row[-1]))])
+                                    for i in range(int(row[-1]))])
 
         sol = np.array(list(map(float, row[:-3])))
         sol_value = q*np.transpose(sol).dot(sigma).dot(sol) - np.transpose(
@@ -66,7 +66,7 @@ with open(set2_filename) as csvfile:
     for row in reader:
         # To append energy num_occur times.
         set2_samples_energy.extend([float(row[-2])
-                                   for i in range(int(row[-1]))])
+                                    for i in range(int(row[-1]))])
 
         sol = np.array(list(map(float, row[:-3])))
         sol_value = q*np.transpose(sol).dot(sigma).dot(sol) - np.transpose(
@@ -77,81 +77,38 @@ with open(set2_filename) as csvfile:
 # 2 columns, 9 per 6 inches figure
 fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(9, 6))
 
-ax1_boxplots_dic = ax1.boxplot([set1_samples_energy, set2_samples_energy], labels=['normal', 'clique'])
 
-ax2_boxplots_dic = ax2.boxplot([set1_samples_value, set2_samples_value], labels=['normal', 'clique'])
+ax1.hist([set1_samples_energy, set2_samples_energy], bins='fd', color=['red', 'blue'], orientation='horizontal', rwidth=1, label=['normal', 'clique'])
 
-# Draw medians
-for line in ax1_boxplots_dic['medians']:
-    # get position data for median line
-    x, y = line.get_xydata()[1]  # top of median line
-    num = line.get_data()[1][1]
-    # overlay median value
-    # draw on right side
-    ax1.text(x, y, f'{num:.2f}', horizontalalignment='left')
+ax2.hist([set1_samples_value, set2_samples_value], bins='fd', color=['red', 'blue'], orientation='horizontal', rwidth=1, label=['normal', 'clique'])
 
-for line in ax2_boxplots_dic['medians']:
-    # get position data for median line
-    x, y = line.get_xydata()[1]  # top of median line
-    num = line.get_data()[1][1]
-    # overlay median value
-    # draw on right side
-    ax2.text(x, y, f'{num:.3f}', horizontalalignment='left')
 
-# Draw outliers
-for line in ax1_boxplots_dic['fliers']:
-    # get position data
-    x, y = line.get_xydata()[0] 
-    # overlay number of outliers
-    num = len(line.get_xydata())
-    # draw on right side
-    ax1.text(x+0.15, y, f'{num}\noutliers', horizontalalignment='left')
-
-for line in ax2_boxplots_dic['fliers']:
-    # get position data
-    x, y = line.get_xydata()[0]
-    # overlay number of outliers
-    num = len(line.get_xydata())
-    # draw on right side
-    ax2.text(x+0.15, y, f'{num}\noutliers', horizontalalignment='left')
-
-# Draw best classical value:
-min_solution_value = q*np.transpose(min_solution).dot(sigma).dot(min_solution) - np.transpose(
-            mu).dot(min_solution) + P * np.square(np.ones((1, N)).dot(min_solution) - B)
-print(min_solution_value[0])
-ax2.axhline(y=min_solution_value[0], color='red', linestyle='-', label='classical best')
-
-# Draw worst classical value:
-max_solution_value = q*np.transpose(max_solution).dot(sigma).dot(max_solution) - np.transpose(
-            mu).dot(max_solution) + P * np.square(np.ones((1, N)).dot(max_solution) - B)
-print(max_solution_value[0])
-ax2.axhline(y=max_solution_value[0], color='darkred', linestyle='-', label='classical worst')
-
-    
 # Tidy up the figure
 ax1.grid(True)
+ax1.legend()
 ax1.set_ylabel('Solution Energy')
-ax1.set_xlabel('Embedding')
+ax1.set_xlabel('Number of Solutions')
 ax2.grid(True)
 ax2.legend()
 ax2.set_ylabel('Solution Objective Value')
-ax2.set_xlabel('Embedding')
+ax2.set_xlabel('Number of Solutions')
 
 # Get timestamp
 date_obj = datetime.now()
 date = 'Y{:04}M{:02}D{:02}h{:02}m{:02}s{:02}'.format(
     date_obj.year, date_obj.month, date_obj.day, date_obj.hour, date_obj.minute, date_obj.second)
 
-folder_name = "boxplots_energy_vs_objective_values"
+folder_name = "histograms_energy_vs_objective_value"
 
 # Check if folder exists and creates if not
 if not os.path.exists('images/' + folder_name):
     os.makedirs('images/' + folder_name)
 
-fig.text(0.5,0.005,'How to interpret: The lower the `y` value, the better. Therefore, lower boxplots suggest better performance.\nMoreover, outliers over the boxplots are undesired. Finally, the lower the mean line, the better.', ha='center', size='xx-small')
+fig.text(0.5, 0.005, 'How to interpret: The lower the value, the better. Hence, the bigger the share of solutions in the lowest bins, the better.',
+         ha='center', size='xx-small')
 
 output_name = f'N{N}B{B}q{q}P{P}W{work_id}{date}'
-fig.suptitle('Boxplots - ' + output_name)
+fig.suptitle('Histograms Energy and Objective Value - ' + output_name)
 
 # Save as 2160p image
 plt.savefig(
--- src/compare_sets_of_samples/scatters_energy_vs_objective_value.py
+++ src/compare_sets_of_samples/scatters_energy_vs_objective_value.py
@@ -52,7 +52,7 @@ with open(set1_filename) as csvfile:
     for row in reader:
         # To append energy num_occur times.
         set1_samples_energy.extend([float(row[-2])
-                                   for i in range(int(row[-1]))])
+                                    for i in range(int(row[-1]))])
 
         sol = np.array(list(map(float, row[:-3])))
         sol_value = q*np.transpose(sol).dot(sigma).dot(sol) - np.transpose(
@@ -66,7 +66,7 @@ with open(set2_filename) as csvfile:
     for row in reader:
         # To append energy num_occur times.
         set2_samples_energy.extend([float(row[-2])
-                                   for i in range(int(row[-1]))])
+                                    for i in range(int(row[-1]))])
 
         sol = np.array(list(map(float, row[:-3])))
         sol_value = q*np.transpose(sol).dot(sigma).dot(sol) - np.transpose(
@@ -75,83 +75,61 @@ with open(set2_filename) as csvfile:
         set2_samples_value.extend([sol_value[0] for i in range(int(row[-1]))])
 
 # 2 columns, 9 per 6 inches figure
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(9, 6))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(9, 6))
 
-ax1_boxplots_dic = ax1.boxplot([set1_samples_energy, set2_samples_energy], labels=['normal', 'clique'])
+vmin_energy = min(min(set1_samples_energy), min(set2_samples_energy))
+vmax_energy = max(max(set1_samples_energy), max(set2_samples_energy))
+vmin_value = min(min(set1_samples_value), min(set2_samples_value))
+vmax_value = max(max(set1_samples_value), max(set2_samples_value))
 
-ax2_boxplots_dic = ax2.boxplot([set1_samples_value, set2_samples_value], labels=['normal', 'clique'])
+print(vmin_energy, vmax_energy)
+print(vmin_value, vmax_value)
 
-# Draw medians
-for line in ax1_boxplots_dic['medians']:
-    # get position data for median line
-    x, y = line.get_xydata()[1]  # top of median line
-    num = line.get_data()[1][1]
-    # overlay median value
-    # draw on right side
-    ax1.text(x, y, f'{num:.2f}', horizontalalignment='left')
+ax1.scatter(list(range(len(set1_samples_energy))),
+            set1_samples_energy, c='red', marker='|')
+ax1.set_ylim(vmin_energy, vmax_energy)
 
-for line in ax2_boxplots_dic['medians']:
-    # get position data for median line
-    x, y = line.get_xydata()[1]  # top of median line
-    num = line.get_data()[1][1]
-    # overlay median value
-    # draw on right side
-    ax2.text(x, y, f'{num:.3f}', horizontalalignment='left')
+ax2.scatter(list(range(len(set1_samples_value))),
+            set1_samples_value, c='red', marker='|')
+ax2.set_ylim(vmin_value, vmax_value)
 
-# Draw outliers
-for line in ax1_boxplots_dic['fliers']:
-    # get position data
-    x, y = line.get_xydata()[0] 
-    # overlay number of outliers
-    num = len(line.get_xydata())
-    # draw on right side
-    ax1.text(x+0.15, y, f'{num}\noutliers', horizontalalignment='left')
+ax3.scatter(list(range(len(set2_samples_energy))),
+            set2_samples_energy, c='blue', marker='|')
+ax3.set_ylim(vmin_energy, vmax_energy)
 
-for line in ax2_boxplots_dic['fliers']:
-    # get position data
-    x, y = line.get_xydata()[0]
-    # overlay number of outliers
-    num = len(line.get_xydata())
-    # draw on right side
-    ax2.text(x+0.15, y, f'{num}\noutliers', horizontalalignment='left')
+ax4.scatter(list(range(len(set2_samples_value))),
+            set2_samples_value, c='blue', marker='|')
+ax4.set_ylim(vmin_value, vmax_value)
 
-# Draw best classical value:
-min_solution_value = q*np.transpose(min_solution).dot(sigma).dot(min_solution) - np.transpose(
-            mu).dot(min_solution) + P * np.square(np.ones((1, N)).dot(min_solution) - B)
-print(min_solution_value[0])
-ax2.axhline(y=min_solution_value[0], color='red', linestyle='-', label='classical best')
 
-# Draw worst classical value:
-max_solution_value = q*np.transpose(max_solution).dot(sigma).dot(max_solution) - np.transpose(
-            mu).dot(max_solution) + P * np.square(np.ones((1, N)).dot(max_solution) - B)
-print(max_solution_value[0])
-ax2.axhline(y=max_solution_value[0], color='darkred', linestyle='-', label='classical worst')
-
-    
 # Tidy up the figure
 ax1.grid(True)
-ax1.set_ylabel('Solution Energy')
-ax1.set_xlabel('Embedding')
+ax1.set_title('Solution Energy')
+ax1.set_ylabel('Normal Embedding')
 ax2.grid(True)
-ax2.legend()
-ax2.set_ylabel('Solution Objective Value')
-ax2.set_xlabel('Embedding')
+ax2.set_title('Solution Objective Value')
+ax3.grid(True)
+ax3.set_xlabel('Solution Index')
+ax3.set_ylabel('Clique Embedding')
+ax4.grid()
+ax4.set_xlabel('Solution Index')
 
 # Get timestamp
 date_obj = datetime.now()
 date = 'Y{:04}M{:02}D{:02}h{:02}m{:02}s{:02}'.format(
     date_obj.year, date_obj.month, date_obj.day, date_obj.hour, date_obj.minute, date_obj.second)
 
-folder_name = "boxplots_energy_vs_objective_values"
+folder_name = "scatters_energy_vs_objective_value"
 
 # Check if folder exists and creates if not
 if not os.path.exists('images/' + folder_name):
     os.makedirs('images/' + folder_name)
 
-fig.text(0.5,0.005,'How to interpret: The lower the `y` value, the better. Therefore, lower boxplots suggest better performance.\nMoreover, outliers over the boxplots are undesired. Finally, the lower the mean line, the better.', ha='center', size='xx-small')
+fig.text(0.5, 0.005, 'How to interpret: The lower the value, the better. Hence, the bigger the share of solutions in the lowest value levels, the better.',
+         ha='center', size='xx-small')
 
 output_name = f'N{N}B{B}q{q}P{P}W{work_id}{date}'
-fig.suptitle('Boxplots - ' + output_name)
+fig.suptitle('Scatters Energy and Objective Value - ' + output_name)
 
 # Save as 2160p image
 plt.savefig(

75525a86314aef6891890459ba0555b620474c9b
Cláudio Gomes <cfpgcp3@gmail.com>
2021-03-27 17:48:10 +0000

    Add dataset types

--- src/stocks_retriever.py
+++ src/stocks_retriever.py
@@ -10,8 +10,60 @@ N = 50
 # TODO: Change method of choosing stocks.
 table = pd.read_html(
     'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-tickers = list(table[0]['Symbol'].sort_values())[:N]
 
+table = table[0]
+print(table[lambda x: x['GICS Sector'] == 'Communication Services'])
+
+tickers = None
+
+dataset_type = 'weakly_correlated'
+
+if dataset_type == 'alphabetical':
+    tickers = list(table['Symbol'].sort_values())[:N]
+elif dataset_type == 'diversified':
+    sectors = list(np.unique(table['GICS Sector']))
+    taboo_subsectors = []
+    tickers = []
+    while len(tickers) < N:
+        for sector in sectors:
+            for index, row in table[lambda x: x['GICS Sector'] == sector].iterrows():
+                if row['Symbol'] not in tickers and row['GICS Sub-Industry'] not in taboo_subsectors:
+                    tickers.append(row['Symbol'])
+                    taboo_subsectors.append(row['GICS Sub-Industry'])
+                    break
+            if len(tickers) == N:
+                break
+elif dataset_type == 'weakly_correlated':
+    sectors = list(np.unique(table['GICS Sector']))
+    desired_subsectors = []
+    tickers = []
+    while len(tickers) < N:
+        for sector in sectors:
+            for index, row in table[lambda x: x['GICS Sector'] == sector].iterrows():
+                if row['Symbol'] not in tickers and row['GICS Sub-Industry'] not in desired_subsectors:
+                    tickers.append(row['Symbol'])
+                    desired_subsectors.append(row['GICS Sub-Industry'])
+                    if len(tickers) == N:
+                        break
+                    tickers.extend(list(table[lambda x: x['GICS Sub-Industry'] == row['GICS Sub-Industry']][lambda x: x['Symbol'] == row['Symbol']]['Symbol']))
+                    if len(tickers) > N:
+                        tickers = tickers[:N]
+                        break
+            if len(tickers) == N:
+                break
+elif dataset_type == 'strongly_correlated':
+    sectors = list(np.unique(table['GICS Sector']))
+    tickers = []
+    while len(tickers) < N:
+        for sector in sectors:
+            for index, row in table[lambda x: x['GICS Sector'] == sector].iterrows():
+                if row['Symbol'] not in tickers:
+                    tickers.append(row['Symbol'])
+                    if len(tickers) == N:
+                        break
+            if len(tickers) == N:
+                break
+print(tickers)
 convert_dot_to_dash_tickers = ['BF.B', 'BRK.B']
 
 for i in range(len(tickers)):
@@ -43,19 +95,22 @@ sigma = data.cov(0)
 print('Sigma:')
 print(sigma)
 
-# Get timestamp
-date_obj = datetime.now()
-date = 'Y{:04}M{:02}D{:02}h{:02}m{:02}s{:02}'.format(
-    date_obj.year, date_obj.month, date_obj.day, date_obj.hour, date_obj.minute, date_obj.second)
+sigma_sum = sigma.to_numpy().sum()
+print(f'{dataset_type}:{sigma_sum}')
 
-# Export data to file
-with open(f'data/out_N{N}_p{period}_i{interval}.json', 'w') as f:
-    json.dump({
-        'N': N,
-        'period': period,
-        'interval': interval,
-        'tickers': tickers,
-        'date': date,
-        'mu': mu.to_dict(),
-        'sigma': sigma.to_dict(),
-    }, fp=f, indent=4)
+# # Get timestamp
+# date_obj = datetime.now()
+# date = 'Y{:04}M{:02}D{:02}h{:02}m{:02}s{:02}'.format(
+#     date_obj.year, date_obj.month, date_obj.day, date_obj.hour, date_obj.minute, date_obj.second)
+
+# # Export data to file
+# with open(f'data/out_{dataset_type}_N{N}_p{period}_i{interval}.json', 'w') as f:
+#     json.dump({
+#         'N': N,
+#         'period': period,
+#         'interval': interval,
+#         'tickers': tickers,
+#         'date': date,
+#         'mu': mu.to_dict(),
+#         'sigma': sigma.to_dict(),
+#     }, fp=f, indent=4)
